7c56514a17641f93b6928c03d2015931b4ca41f1
Luca Azalim <lucaazalim@gmail.com>
2023-06-04 16:15:25 -0300

    Updating string lengths for real world values.

--- src/constants.h
+++ src/constants.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define CUSTOMER_NAME_LENGTH 32
-#define ADDRESS_LENGTH 64
+#define CUSTOMER_NAME_LENGTH 64
+#define ADDRESS_LENGTH 128
 #define PHONE_LENGTH 11
 #define SUPPLIER_NAME_LENGTH 64
-#define PARTY_THEME_LENGTH 32
+#define PARTY_THEME_LENGTH 64
 
 #define CUSTOMER_DATA_FILENAME "data/customer.dat"
 #define SUPPLIER_DATA_FILENAME "data/supplier.dat"
